deec6483a5daf782a48d19dd36b1f26f4a87f260
rikuto-yasunaka <67901951+rikuto-yasunaka@users.noreply.github.com>
2021-11-09 18:44:56 +0900

    Update settings.py

--- sns/settings.py
+++ sns/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j)_j0i(3#w2kql8^24_s7*(sl9+t4nzioh@u2ox#5s6n(li8xv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
